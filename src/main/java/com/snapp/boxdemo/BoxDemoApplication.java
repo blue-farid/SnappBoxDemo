3bbf33bc07cc5a569208c09428c57636d06de5f4
blue_farid <blue_farid@hotmail.com>
2022-11-19 13:46:53 +0330

    initial commit

---
+++ src/main/java/com/snapp/boxdemo/BoxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.snapp.boxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snapp/boxdemo/BoxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapp.boxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
