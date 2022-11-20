ca3d89c43aeda36f5ac8db33b23f9ac014d384e8
blue_farid <blue_farid@hotmail.com>
2022-11-20 14:12:59 +0330

    add BoxOrderController

---
+++ src/main/java/com/snapp/boxdemo/controller/BoxOrderController.java
@@ -0,0 +1,32 @@
+package com.snapp.boxdemo.controller;
+
+import com.snapp.boxdemo.dto.BoxOrderDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/order")
+public class BoxOrderController {
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<BoxOrderDto> getBoxOrder(@PathVariable Long orderId) {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postBoxOrder(@RequestBody BoxOrderDto dto) {
+        return null;
+    }
+
+    @PutMapping
+    public ResponseEntity<String> putBoxOrder(@RequestBody BoxOrderDto dto) {
+        return null;
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<BoxOrderDto> deleteBoxOrder(@PathVariable Long orderId) {
+        return null;
+    }
+}
